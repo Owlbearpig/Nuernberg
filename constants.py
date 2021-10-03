9823e928d85fa8914ad0fca8e9cec49c999a85ce
Owlbearpig <alexj07@gmail.com>
2021-10-03 21:05:55 +0200

    -0

--- constants.py
+++ constants.py
@@ -6,3 +6,5 @@ painting_photo_path = Path(r'G:\Nuremberg2021\DBU_Nuernberg_JanLaptop\GemaeldeNu
 
 data_dir_part1 = Path(r'C:\Users\Kochbuch\Desktop\NuernbergLaptop\Part1\PaintingNuernberg')
 data_dir_part2 = Path(r'G:\Nuremberg2021')
+
+data_dir_day4_nail = Path(r'E:\CURPROJECT\measurementdata\Nuernberg\Phase2\Day4_X+0_Y+267_NailScan')
